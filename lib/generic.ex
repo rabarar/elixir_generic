5d045a34c474878cf7b808d38e6ea3cbffe100a4
Robert Baruch <robertbaruch@mac.com>
2016-12-24 22:17:36 +0000

    updated Generic

--- lib/generic.ex
+++ lib/generic.ex
@@ -1,14 +1,12 @@
 defmodule Generic do
 
   def run_generic do
-    Generic.Registry.start_link
     Generic.Supervisor.start_link(:sup1, %{worker: Generic.Server, opts: %{}})
     Generic.Supervisor.start_server(:sup1, "s1")
   end
 
   def run_tcp do
-    Generic.Registry.start_link
-    Generic.Supervisor.start_link(:sup2, %{worker: TCPServer, opts: %{port: 4000, service: "dothis"}})
-    Generic.Supervisor.start_server(:sup2, "s2")
+    Generic.Supervisor.start_link(:sup2, %{worker: TCPServer, opts: %{port: 4000, service: TCP.Service.A}})
+    Generic.Supervisor.start_server(:sup2, "serverA")
   end
 end
